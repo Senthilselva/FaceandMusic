9dc3e821db749f967c58eb8e198a872c8a92b917
Michael Constanza <michael.constanza@gmail.com>
2016-09-28 23:04:31 -0400

    preparing to merge new filepicker code

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -6,25 +6,6 @@ $(document).ready(function() {
 
 	checkAuthorization();
 
-	$('#btnLogin').on('click', function(){
-		OAuthManager.obtainToken({
-          scopes: [
-            /*
-              the permission for reading public playlists is granted
-              automatically when obtaining an access token through
-              the user login form
-              */
-              'playlist-read-private',
-              'playlist-read-collaborative',
-              'playlist-modify-public',
-              'playlist-modify-private'
-            ]
-          }).then(function(token) {
-            onTokenReceived(token);
-          }).catch(function(error) {
-            console.error(error);
-          });
-	});
 
 
     // function to search spotify api for playlists
