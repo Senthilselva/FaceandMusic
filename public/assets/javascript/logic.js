444714ccad97ecd4068e12ee4ee53e788f5f42b6
Michael Constanza <michael.constanza@gmail.com>
2016-09-29 00:50:06 -0400

    moar pushing.

--- public/assets/javascript/logic.js
+++ public/assets/javascript/logic.js
@@ -156,47 +156,6 @@ function getUserInfo(user){
 
 }//getUserInfo
 
-// $("#btnLogin").on('click',function(){
-// 	var emailV=$("#txtEmail").val();
-//   if(!checkEmail(emailV)){
-//     return false;
-//   }
-//   var passwordV = $("#txtPassword").val();
-//   if(!checkPassword(passwordV)){
-//     return false;
-//   }
-
-//   //sign in with the user
-// 	const promise = auth.signInWithEmailAndPassword(emailV,passwordV)
-// 	promise.then(user => getUserInfo(user));
-//   promise.catch(e => 
-//     $("#errMsgSignUp").html(e.message));
-// });
-
-// //-------get User info from database--------
-// function getUserInfo(user){
-//   database.ref('Members').orderByChild("email").equalTo(user.email).once('value',function(snapshot){
-// //    console.log("snapshot "+ JSON.stringify(snapshot.val()));
-//     snapshot.forEach(function(data) {
-//       var nameV = changetoCapitol(data.val().name);
-//       $('.usernameDisplay').html(nameV);
-//         console.log("Name: "+ data.val().name);
-//         console.log("email: "+ data.val().email);
-//         gvUser.email=data.val().email;
-//         gvUser.name=data.val().name;
-//         gvUser.key=data.key;
-//         console.log("key "+gvUser.key);
-//         if(data.child("Picture").exists()){
-//           console.log("picture");
-//           data.child("Picture").forEach(function(picPath) {
-//             console.log(picPath.val());
-//             });
-//         }//if picture exists
-//     }); //looping through the snapshot  
-
-//   });//database query
-
-// }//getUserInfo
 
 
 //change the first letter after space to capital
@@ -237,11 +196,6 @@ $("#btnLogout").on('click',function(){
 });
 	
 
-//$("#picInputFile").on('change',previewFile);
-
-////Display on page
-
-
 $("#storedImages").on("click",'.picture', function(e){
   downLoadFile(e)
 });
@@ -251,52 +205,6 @@ var url =$(e.target).data("url");
 //Call emotive here/////////////////
 callEmotive(url, apiUrl, apiKey);
 }
-
-// //Show and hide the login and 
-// firebase.auth().onAuthStateChanged(firebaseUser => {
-// 		if(firebaseUser){
-//       $('#mainPage').show();
-//       $('#LoginAndSignUP').hide();
-//       getUserInfo(firebaseUser);
-// 			console.log(firebaseUser);
-// 		} else {
-//       gvEmail="";
-//       $('#mainPage').hide();
-//       $('#LoginAndSignUP').show();
-// 			console.log("not logged in");
-// 		}
-// });//Closing OnAuthSateChanged
-
-// $("#btnLogout").on('click',function(){
-// 	firebase.auth().signOut();
-//   location.reload();
-// });
-	
-
-
-
-// $("#btnUpload").on("click",downLoadFile);
-
-// function downLoadFile(){
-
-// var picname="technics.jpg"
-//   storageref.child("senthil/"+picname).getDownloadURL().then(function(url) {
-// console.log("downloading"+ url);
-    
-//     var uploadImg = document.querySelector('img');
-//     uploadImg.src=url;
-    
-//     emotive(newfile);
-
-//   });
-
-  
-// }
-
-/////emotive stuff
-
-
-
 
 
 function previewFile(url) {
@@ -310,42 +218,3 @@ function previewFile(url) {
   console.log(file)
   callEmotive(file, apiUrl, apiKey);
 }
-
-
-
- // $('#btn').click(function () {
- // //file: The file that will be sent to the api
- // var file = document.getElementById('filename').files[0];
- 
- // emotiveAPI(file, apiUrl, apiKey);
- // });
- 
- // function emotive(file){
- //  console.log("hahaha");
- // }
-
- ////Display on page
-// function previewFile() {
-//     //console.log("preview")
-//   var preview = $('.uploadImage');
-//   var file    = document.querySelector('input[type=file]').files[0];
-//   var reader  = new FileReader();
-//   console.log(file);
-
-//   reader.addEventListener("load", function () {
-//     preview.attr('src',reader.result);
-//   }, false);
-
-//   if(file){
-//    reader.readAsDataURL(file);
-//     //uploading to firebase storage
-//     storageref.child(gvUser.email+"/"+file.name).put(file);
-//   console.log(gvUser.email+"/"+file.name);
-//   console.log(gvUser.key);
-//   //saving it to the database
-//   database.ref("Members").child(gvUser.key+"/Picture").push({pic: gvUser.email+"/"+file.name});
-//   callEmotive(file,apiUrl,apiKey);
-//   }
-// }
- 
-
